f2d9bdb22a5fc9453a8b382a293e1b9247f2bfad
NataliaColome <nmcolome@gmail.com>
2020-03-18 13:37:46 -0600

    Change shelter model spec to include validation msg

--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe Shelter, type: :model do
   end
 
   describe "validations" do
-    it { should validate_presence_of :name }
-    it { should validate_presence_of :address }
-    it { should validate_presence_of :city }
-    it { should validate_presence_of :state }
-    it { should validate_presence_of :zip }
+    it { should validate_presence_of(:name).with_message('Field required') }
+    it { should validate_presence_of(:address).with_message('Field required') }
+    it { should validate_presence_of(:city).with_message('Field required') }
+    it { should validate_presence_of(:state).with_message('Field required') }
+    it { should validate_presence_of(:zip).with_message('Field required') }
   end
 
   describe "relationships" do
