20e88e3e46b5254ba300d05f39dd27c4b5bcdbbc
NataliaColome <nmcolome@gmail.com>
2020-03-19 15:36:45 -0600

    Modify seed file to create 5 pets per shelter

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Shelter.destroy_all
 Pet.destroy_all
+Shelter.destroy_all
 
 names = ["Pet Society", "Animal Shelter", "Animal Rescue", "Pet Avengers", "Friendly Animal Neighbour", "Animal Society", "Dog Shelter", "Lend a Helping Paw", "Cat Shelter"]
 
@@ -21,11 +21,11 @@ names.each do |name|
                 )
 end
 
-Shelter.all.each_with_index do |shelter, index|
-  5.times do
+Shelter.all.each do |shelter|
+  5.times do |index|
     dog_name = Faker::Creature::Dog.name
     shelter.pets.create!(
-                          image: "https://placedog.net/500?id=#{index}",
+                          image: "https://placedog.net/500?id=#{index + 1}",
                           name: dog_name,
                           description: "What our staff says: #{Faker::Creature::Dog.meme_phrase}! #{dog_name} is a #{Faker::Creature::Dog.size} #{Faker::Creature::Dog.breed} with a #{Faker::Creature::Dog.coat_length} coat and a very friendly disposition. #{dog_name} loves long walks, listening to npr, going '#{Faker::Creature::Dog.sound}'' & chillin'.",
                           approximate_age: Random.rand(16),
