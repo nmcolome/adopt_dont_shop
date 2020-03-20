0a7f013dcdea813f2daf9cab7570bdfc46101423
NataliaColome <nmcolome@gmail.com>
2020-03-20 14:56:31 -0600

    Add validation spec & validations for Pet

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,6 +1,8 @@
 class Pet < ApplicationRecord
   belongs_to :shelter
 
+  validates_presence_of :image, :name, :description, :approximate_age, :sex, :shelter_id, message: "Field required"
+
   enum sex: [:female, :male]
   enum status: [:adoptable, :pending]
 end
