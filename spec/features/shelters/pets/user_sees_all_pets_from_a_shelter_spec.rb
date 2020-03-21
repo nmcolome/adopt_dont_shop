4ce171b7fc5138fbc8610c78f823ebd372cdf1d9
NataliaColome <nmcolome@gmail.com>
2020-03-21 00:52:26 -0600

    Add feature spec for Pet Filter by Adoptable Status

--- spec/features/shelters/pets/user_sees_all_pets_from_a_shelter_spec.rb
+++ spec/features/shelters/pets/user_sees_all_pets_from_a_shelter_spec.rb
@@ -45,4 +45,32 @@ RSpec.describe "Shelter Pets Index" do
       expect(page).to have_css("a.pet-link:last-child", text: @last_pet.name)
     end
   end
+
+  context "Pet Filter by Adoptable Status" do
+    it "user selects to show only adoptable pets" do
+      visit shelter_pets_path(@shelter)
+
+      click_on "Only Adoptable Pets"
+
+      expect(current_path).to eq('/pets?adoptable=true')
+      expect(page).to have_content(@pets[0].name)
+      expect(page).to have_css("div.pet-card", count: 6)
+      expect(page).to_not have_content(@last_pet.name)
+      expect(page).to have_link("All Pets")
+      expect(page).to have_link("Only Adoption-Pending Pets")
+    end
+
+    it "user selects to show only adoption-pending pets" do
+      visit shelter_pets_path(@shelter)
+
+      click_on "Only Adoption-Pending Pets"
+
+      expect(current_path).to eq('/pets?adoptable=false')
+      expect(page).to have_content(@last_pet.name)
+      expect(page).to_not have_content(@pets[0].name)
+      expect(page).to have_css("div.pet-card", count: 1)
+      expect(page).to have_link("All Pets")
+      expect(page).to have_link("Only Adoptable Pets")
+    end
+  end
 end
