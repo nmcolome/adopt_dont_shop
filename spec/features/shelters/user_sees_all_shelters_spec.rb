525b0ac440ac1b9712f6f3f0abd7b12aa2174915
NataliaColome <nmcolome@gmail.com>
2020-03-21 20:29:19 -0600

    Add model specs, links, & methods to sort shelters by adoptable pets or by alphabet

--- spec/features/shelters/user_sees_all_shelters_spec.rb
+++ spec/features/shelters/user_sees_all_shelters_spec.rb
@@ -10,35 +10,32 @@ RSpec.describe "Shelter's index" do
   end
 
   context "User can sort shelters" do
-    it "by number of adoptable pets" do
-      shelters_a = create(:shelter, name: "ABC Shelter")
-      pets_a = create_list(:pets, 3, shelter_id: shelters_a.id)
-      shelters_z = create(:shelter, name: "Zootopia Shelter")
-      pets_z = create_list(:pets, 5, shelter_id: shelters_z.id)
-      shelter_p = create(:shelter, name: "Phoenix Shelter")
-      pets_p = create_list(:pets, 8, shelter_id: shelters_p.id)
+    before(:each) do
+      @shelter_a = create(:shelter, name: "ABC Shelter")
+      pets_a = create_list(:pet, 3, shelter_id: @shelter_a.id)
+      @shelter_z = create(:shelter, name: "Zootopia Shelter")
+      pets_z = create_list(:pet, 5, shelter_id: @shelter_z.id)
+      @shelter_p = create(:shelter, name: "Phoenix Shelter")
+      pets_p = create_list(:pet, 8, shelter_id: @shelter_p.id)
+    end
 
+    it "by number of adoptable pets" do
       visit shelters_path
+      click_on "Sort by Total Adoptable Pets"
 
       within ".container" do
-        expect(page).to have_css("a.index-link:first-child", text: shelters_p.name)
-        expect(page).to have_css("a.index-link:last-child", text: shelters_a.name)
+        expect(page).to have_css("a.index-link:first-child", text: @shelter_p.name)
+        expect(page).to have_css("a.index-link:last-child", text: @shelter_a.name)
       end
     end
 
-    skip it "in alphabetical order" do
-      shelters_a = create(:shelter, name: "ABC Shelter")
-      pets_a = create_list(:pets, 3, shelter_id: shelters_a.id)
-      shelters_z = create(:shelter, name: "Zootopia Shelter")
-      pets_z = create_list(:pets, 5, shelter_id: shelters_z.id)
-      shelter_p = create(:shelter, name: "Phoenix Shelter")
-      pets_p = create_list(:pets, 8, shelter_id: shelters_p.id)
-
+    it "in alphabetical order" do
       visit shelters_path
+      click_on "Sort in Alphabetical Order"
 
       within ".container" do
-        expect(page).to have_css("a.index-link:first-child", text: shelters_a.name)
-        expect(page).to have_css("a.index-link:last-child", text: shelters_z.name)
+        expect(page).to have_css("a.index-link:first-child", text: @shelter_a.name)
+        expect(page).to have_css("a.index-link:last-child", text: @shelter_z.name)
       end
     end
   end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe Shelter, type: :model do
   describe "relationships" do
     it { should have_many :pets }
   end
+
+  describe "methods" do
+    before(:each) do
+      @shelter_a = create(:shelter, name: "ABC Shelter")
+      pets_a = create_list(:pet, 3, shelter_id: @shelter_a.id)
+      @shelter_z = create(:shelter, name: "Zootopia Shelter")
+      pets_z = create_list(:pet, 5, shelter_id: @shelter_z.id)
+      @shelter_p = create(:shelter, name: "Phoenix Shelter")
+      pets_p = create_list(:pet, 8, shelter_id: @shelter_p.id)
+    end
+
+    it "#sort_by_adoptable_pets" do
+      expect(Shelter.sort_by_adoptable_pets).to eq([@shelter_p, @shelter_z, @shelter_a])
+    end
+
+    it "#sort_by_name" do
+      expect(Shelter.sort_by_name).to eq([@shelter_a, @shelter_p, @shelter_z])
+    end
+  end
 end
